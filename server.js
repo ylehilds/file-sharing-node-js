1f8b34a379a7c246a8f81f202e38e73fbb0537ae
ylehilds <lehi_alcantara@byu.edu>
2023-03-26 01:42:53 -0600

    finished implementation

---
+++ server.js
@@ -0,0 +1,63 @@
+require('dotenv').config() // this reads from the root folder a .env file with these 2 lines:
+// DATABASE_URL='mongodb://127.0.0.1/fileSharing'
+// PORT=3000
+
+const multer = require('multer')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const File = require('./models/Files')
+
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({extended: true}))
+
+const upload = multer({dest: 'uploads'})
+
+mongoose.connect(process.env.DATABASE_URL) //DATABASE_URL='mongodb://127.0.0.1/fileSharing'
+
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname
+  }
+  if (req.body.password !== null && req.body.password !== '') {
+    fileData.password = await bcrypt.hash(req.body.password, 10)
+  }
+
+  const file = await File.create(fileData)
+  res.render('index', {fileLink: `${req.headers.origin}/file/${file.id}`})
+})
+
+app.route('/file/:id').get(handleDownload).post(handleDownload)
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id)
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render('password')
+      return
+    }
+    if (!await bcrypt.compare(req.body.password, file.password)) {
+      res.render('password', {error: true})
+      return
+    }
+  }
+
+  file.downloadCount++
+  await file.save()
+  console.log(file.downloadCount)
+
+  res.download(file.path, file.originalName)
+}
+
+app.listen(process.env.PORT || 3000) // PORT=3000
+
+// .env file contents, I'm putting it here just because this is a tutorial and these are not really secrets. This will make the code more complete
+// DATABASE_URL='mongodb://127.0.0.1/fileSharing'
+// PORT=3000
